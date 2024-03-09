f1bfff35e37a837fca5f0e79323fd217c4665614
Khalexiii <144989632+Khalleep@users.noreply.github.com>
2024-03-09 16:58:39 +0200

    Update server.lua
    logs

--- server/server.lua
+++ server/server.lua
@@ -9,4 +9,24 @@ AddEventHandler('khalexi_atmrob:payout', function()
         type = 'success',
         description = locale('yougot', amount)
     })
+
+    -- Discord Webhook Logging
+    local webhookData = {
+        content = "**KXI ATM Robbery Payout:**",
+        embeds = {
+            {
+                title = "Details",
+                description = "ATM robbery reported.",
+                fields = {
+                    {name = "Player ID", value = tostring(src)},
+                    {name = "Amount", value = tostring(amount)},
+                    {name = "Reward Item", value = tostring(K.Reward)}
+                },
+                color = 000080
+            }
+        }
+    }
+
+    PerformHttpRequest('YOUR_DISCORD_WEBHOOK_URL_HERE', function(err, text, headers) end, 'POST', json.encode(webhookData), { ['Content-Type'] = 'application/json' })
+
 end)
